f967eaf6bd078862728317a4d42aaa67eebc093a
Jay168 <31158644+Jay168@users.noreply.github.com>
2017-09-02 15:49:04 +0530

    Add files via upload

---
+++ prog7.py
@@ -0,0 +1,11 @@
+#printing variables with strings
+
+likes = raw_input("What do you like\n")
+hates = raw_input("What do you hate\n")
+
+print "Jai likes :", likes
+print "Jai hates :", hates
+
+'''
+Jai likes : 
+'''
